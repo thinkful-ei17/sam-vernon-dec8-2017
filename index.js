506a4601b1256e72ea300610f5ca16468ac8cdef
Sam Gould <sjg326@gmail.com>
2017-12-08 12:37:36 -0800

    Completed update item function

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const STORE = [
-  {name: "apples", checked: false, edit: false},
-  {name: "oranges", checked: false, edit: false},
-  {name: "milk", checked: true, edit: false},
-  {name: "bread", checked: false, edit: false}
+  {name: 'apples', checked: false, edit: false},
+  {name: 'oranges', checked: false, edit: false},
+  {name: 'milk', checked: true, edit: false},
+  {name: 'bread', checked: false, edit: false}
 ];
 
 
@@ -12,12 +12,13 @@ function generateItemElement(item, itemIndex, template) {
   return `
     <li class="js-item-index-element" data-item-index="${itemIndex}">
     ${item.edit ?
-      `<form id="js-shopping-update">
-      <input type="text" value=${item.name} name="shopping-edit-entry" class="js-shopping-edit-entry">
+      
+    `<form id="js-shopping-update">
+      <input type="text" value=${item.name} name="shopping-edit-entry" class="js-shopping-edit-entry" />
       <button type="submit">Update</button>
       </form>`
     :
-     `<span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>`}
+    `<span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>`}
 
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
@@ -33,11 +34,11 @@ function generateItemElement(item, itemIndex, template) {
 
 
 function generateShoppingItemsString(shoppingList) {
-  console.log("Generating shopping list element");
+  console.log('Generating shopping list element');
 
   const items = shoppingList.map((item, index) => generateItemElement(item, index));
 
-  return items.join("");
+  return items.join('');
 }
 
 
@@ -68,7 +69,7 @@ function handleNewItemSubmit() {
 }
 
 function toggleCheckedForListItem(itemIndex) {
-  console.log("Toggling checked property for item at index " + itemIndex);
+  console.log('Toggling checked property for item at index ' + itemIndex);
   STORE[itemIndex].checked = !STORE[itemIndex].checked;
 }
 
@@ -81,7 +82,7 @@ function getItemIndexFromElement(item) {
 }
 
 function handleItemCheckClicked() {
-  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+  $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     console.log('`handleItemCheckClicked` ran');
     const itemIndex = getItemIndexFromElement(event.currentTarget);
     toggleCheckedForListItem(itemIndex);
@@ -90,7 +91,7 @@ function handleItemCheckClicked() {
 }
 
 function deleteSelectedItem(array, index){
-  console.log(`deleteSelectedItem has been clicked at ${index}`)
+  console.log(`deleteSelectedItem has been clicked at ${index}`);
   array.splice(index, 1);
 }
 
@@ -120,40 +121,48 @@ function handleDeleteItemClicked() {
   // line 36 called at line 100
 
 }
+
+function updateItem(array, index, updatedName){
+  array[index].name = updatedName;
+}
+
 function handleEditInputSubmit() {
-  $('.js-shopping-list').on("submit","#js-shopping-update", function(event) {
+  $('.js-shopping-list').on('submit','#js-shopping-update', function(event){
     event.preventDefault();
     console.log('`handleEditInputSubmit` ran');
-    // const newItemName = $('.js-shopping-list-entry').val();
-    // $('.js-shopping-list-entry').val('');
-    // addItemToShoppingList(newItemName);
-    // renderShoppingList();
+    const updatedItemName = $('.js-shopping-edit-entry').val();
+    console.log(`The updated value is ${updatedItemName}`);
+    const itemIndex = getItemIndexFromElement(event.currentTarget);
+    console.log(`The index is ${itemIndex}`);
+    updateItem(STORE, itemIndex, updatedItemName);
+    toggleEditKey(STORE, itemIndex);
+    renderShoppingList();
   });
 }
 
 function toggleEditKey(array, index){
-   array[index].edit = !array[index].edit
+  array[index].edit = !array[index].edit;
 }
 
 function handleEditItemClicked(index) {
   // User can edit an item title
 
-    // User clicks item name
-    $('.js-shopping-list').on('click','.js-shopping-item', function(event){
-      console.log("handleEditItemClicked ran");
-      // Generates an input bar in place of item name with value set to item name and an update item button next to input bar -- Re-render shopping list, change input(our key) to true
-      //edited the generateItemElement + added edit key
-      //toggleEditkey
-      const itemIndex = getItemIndexFromElement(event.currentTarget);
+  // User clicks item name
+  $('.js-shopping-list').on('click','.js-shopping-item', function(event){
+    console.log('handleEditItemClicked ran');
+    // Generates an input bar in place of item name with value set to item name and an update item button next to input bar -- Re-render shopping list, change input(our key) to true
+    //edited the generateItemElement + added edit key
+    //toggleEditkey
+    const itemIndex = getItemIndexFromElement(event.currentTarget);
 
-      toggleEditKey(STORE,itemIndex);
-      // User is able to edit value
+    toggleEditKey(STORE,itemIndex);
+    // User is able to edit value
 
-      // User can submit input
+    // User can submit input
 
-      // Re-render shopping list with input value as new Item Name, input bar and button turned off
-      renderShoppingList();
-    })
+    // Re-render shopping list with input value as new Item Name, input bar and button turned off
+    renderShoppingList();
+  });
 
 }
 
@@ -168,7 +177,7 @@ function handleShoppingList() {
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleEditItemClicked();
-
+  handleEditInputSubmit();
   handleDeleteItemClicked();
 }
 
